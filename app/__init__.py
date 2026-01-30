514ca23054c59a843d21f4758cb02ad16a00d118
Carson Brown <carson@techentum.com>
2026-01-30 13:40:06 -0800

    added migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -14,6 +15,7 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+    migrate = Migrate(app, db)
     login_manager.init_app(app)
 
     from app.routes import auth_bp, mod_bp
